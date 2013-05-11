915515c10eef75d443d035856f02b5aa6b77cb6b
Dejan Fajfar <dejan@fajfar.com>
2013-05-11 15:59:41 +0200

    Changed the API

--- test/com/fajfar/hangen/java/Foo.java
+++ test/com/fajfar/hangen/java/Foo.java
@@ -1,4 +1,10 @@
 package com.fajfar.hangen.java;
 
+import com.fajfar.hangen.java.data.SimpleClassA;
+
 public class Foo {
+
+    TransformationProvider provider = null;
+
+    SimpleClassA destination = provider.from(null);
 }
